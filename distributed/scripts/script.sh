d7dabcf7daa931e542468b4930ffa23ed7b55d7b
Ubuntu <ubuntu@ip-10-0-0-241.ap-northeast-1.compute.internal>
2018-10-31 10:01:49 +0000

    aws跨国境双机调试通过（东京）
    Signed-off-by: Ubuntu <ubuntu@ip-10-0-0-241.ap-northeast-1.compute.internal>
    
    Signed-off-by: JulienGuo(郭立冬-guolidong) <guo_lidong@foxmail.com>

--- distributed/scripts/script.sh
+++ distributed/scripts/script.sh
@@ -88,9 +88,16 @@ installChaincode 0 1
 echo "Install chaincode on peer0.org2..."
 installChaincode 0 2
 
+## Install chaincode on peer1.org2
+echo "Installing chaincode on peer1.org1..."
+installChaincode 1 1
+## Install chaincode on peer1.org2
+echo "Installing chaincode on peer1.org2..."
+installChaincode 1 2
+
 # Instantiate chaincode on peer0.org2
 echo "Instantiating chaincode on peer0.org2..."
-instantiateChaincode 0 2
+instantiateChaincode 1 2
 
 # Query chaincode on peer0.org1
 echo "Querying chaincode on peer0.org1..."
@@ -100,13 +107,11 @@ chaincodeQuery 0 1 100
 echo "Sending invoke transaction on peer0.org1 peer0.org2..."
 chaincodeInvoke 0 1 0 2
 
-## Install chaincode on peer1.org2
-echo "Installing chaincode on peer1.org2..."
-installChaincode 1 2
+
 
 # Query on chaincode on peer1.org2, check if the result is 90
-echo "Querying chaincode on peer1.org2..."
-chaincodeQuery 1 2 90
+#echo "Querying chaincode on peer1.org2..."
+#chaincodeQuery 1 2 90
 
 echo
 echo "========= All GOOD, BYFN execution completed =========== "
