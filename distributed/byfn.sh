c5450ddbe6dc86ee4c472ebee300a3b28101d643
linson <zslin007@qq.com>
2018-11-27 16:31:30 +0800

    为了方便调试，删除证书和通道的脚本先注释掉
    Signed-off-by: JulienGuo(郭立冬-guolidong) <guo_lidong@foxmail.com>

--- distributed/byfn.sh
+++ distributed/byfn.sh
@@ -178,7 +178,7 @@ function networkDown() {
     #Cleanup images
     removeUnwantedImages
     # remove orderer block and other channel configuration transactions and certs
-    rm -rf channel-artifacts/*.block channel-artifacts/*.tx crypto-config
+    #rm -rf channel-artifacts/*.block channel-artifacts/*.tx crypto-config
   fi
 }
 
